5f5e6bd0fd64d684c06606a190e607f2f03a1ccb
Andy Dayton <amdayton@gmail.com>
2016-02-09 16:55:41 -0500

    Update title

--- html.js
+++ html.js
@@ -1,4 +1,4 @@
 var html = require('simple-html-index')
 	, pkg = require('./package.json');
 
-html({title: pkg.title + '' + pkg.version, entry: 'bundle.js', css: 'styles.css'}).pipe(process.stdout);
+html({title: pkg.name + ' ' + pkg.version, entry: 'bundle.js', css: 'styles.css'}).pipe(process.stdout);

faf90104f621ff1986d0aaef94e818073f957c9c
Andy Dayton <amdayton@gmail.com>
2016-02-10 00:24:20 -0500

    Updated to use budo-demo for build

--- html.js
+++
@@ -1,4 +0,0 @@
-var html = require('simple-html-index')
-	, pkg = require('./package.json');
-
-html({title: pkg.name + ' ' + pkg.version, entry: 'bundle.js', css: 'styles.css'}).pipe(process.stdout);
